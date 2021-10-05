2930d8215567cdf6ce8e24ba12e18959558631ba
lucag <lucag@DESKTOP-1MHAK99>
2021-10-04 23:40:44 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Te_juzgo_el_helado
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbSabores = new System.Windows.Forms.ComboBox();
+            this.lblSeleccionar = new System.Windows.Forms.Label();
+            this.btJuzgar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbSabores
+            // 
+            this.cmbSabores.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSabores.FormattingEnabled = true;
+            this.cmbSabores.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.cmbSabores.Location = new System.Drawing.Point(33, 96);
+            this.cmbSabores.Name = "cmbSabores";
+            this.cmbSabores.Size = new System.Drawing.Size(236, 23);
+            this.cmbSabores.TabIndex = 0;
+            this.cmbSabores.SelectedIndexChanged += new System.EventHandler(this.cmbSabores_SelectedIndexChanged);
+            // 
+            // lblSeleccionar
+            // 
+            this.lblSeleccionar.AutoSize = true;
+            this.lblSeleccionar.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSeleccionar.Location = new System.Drawing.Point(33, 32);
+            this.lblSeleccionar.Name = "lblSeleccionar";
+            this.lblSeleccionar.Size = new System.Drawing.Size(340, 30);
+            this.lblSeleccionar.TabIndex = 1;
+            this.lblSeleccionar.Text = "¡Elige tu sabor de helado favorito!";
+            // 
+            // btJuzgar
+            // 
+            this.btJuzgar.Location = new System.Drawing.Point(286, 96);
+            this.btJuzgar.Name = "btJuzgar";
+            this.btJuzgar.Size = new System.Drawing.Size(81, 25);
+            this.btJuzgar.TabIndex = 3;
+            this.btJuzgar.Text = "Juzgar";
+            this.btJuzgar.UseVisualStyleBackColor = true;
+            this.btJuzgar.Click += new System.EventHandler(this.btJuzgar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(420, 99);
+            this.lblResultado.MaximumSize = new System.Drawing.Size(250, 0);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 15);
+            this.lblResultado.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btJuzgar);
+            this.Controls.Add(this.lblSeleccionar);
+            this.Controls.Add(this.cmbSabores);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbSabores;
+        private System.Windows.Forms.Label lblSeleccionar;
+        private System.Windows.Forms.Button btJuzgar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Te_juzgo_el_helado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbSabores.Items.Add("Banana Split");
+            cmbSabores.Items.Add("Bananita Dolca");
+            cmbSabores.Items.Add("Chocolate");
+            cmbSabores.Items.Add("Chocolate con almendras");
+            cmbSabores.Items.Add("Crema cookie");
+            cmbSabores.Items.Add("Crema del cielo");
+            cmbSabores.Items.Add("Durazno");
+            cmbSabores.Items.Add("Frutilla");
+            cmbSabores.Items.Add("Granizado");
+            cmbSabores.Items.Add("Limón");
+            cmbSabores.Items.Add("Marroc");
+            cmbSabores.Items.Add("Papa frita");
+            cmbSabores.Items.Add("Quinotos al Whisky");
+            cmbSabores.Items.Add("Vainilla");
+            cmbSabores.Items.Add("Otro");
+        }
+
+        private void cmbSabores_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btJuzgar_Click(object sender, EventArgs e)
+        {
+            if (cmbSabores.Text== "Banana Split")
+            {
+                lblResultado.Text = "???????";
+            }
+            if (cmbSabores.Text == "Bananita Dolca")
+            {
+                lblResultado.Text = "No sé si existe pero si sabes donde hay por favor llevame.";
+            }
+            if (cmbSabores.Text == "Chocolate")
+            {
+                lblResultado.Text = "No salís de tu zona de confort, deberías arriesgarte más.";
+            }
+            if (cmbSabores.Text == "Chocolate con almendras")
+            {
+                lblResultado.Text = "Respetable.";
+            }
+            if (cmbSabores.Text == "Crema cookie")
+            {
+                lblResultado.Text = "Trolazo, pero same bro.";
+            }
+            if (cmbSabores.Text == "Crema del cielo")
+            {
+                lblResultado.Text = "Sos fan de las películas disney. Sobre todo de Mulan.";
+            }
+            if (cmbSabores.Text == "Durazno")
+            {
+                lblResultado.Text = "Alguien fresco, seguro lo pedís con otros 3 sabores frutales.";
+            }
+            if (cmbSabores.Text == "Frutilla")
+            {
+                lblResultado.Text = "Te diría algo pero la verdad suficiente triste tiene que ser tu vida para que eso sea tu sabor favorito.";
+            }
+            if (cmbSabores.Text == "Granizado")
+            {
+                lblResultado.Text = "Casate conmigo por favor no importa que yo sea un programa y vos un humano.";
+            }
+            if (cmbSabores.Text == "Limón")
+            {
+                lblResultado.Text = "Te gustaba más el helado de limon de antes pero aún así te gusta.";
+            }
+            if (cmbSabores.Text == "Marroc")
+            {
+                lblResultado.Text = "Eso es trampa, todos amamos el Marroc. (Espacio no auspiciado por Felfort)";
+            }
+            if (cmbSabores.Text == "Papa frita")
+            {
+                lblResultado.Text = "No existe, pero debería.";
+            }
+            if (cmbSabores.Text == "Quinotos al Whisky")
+            {
+                DecidisteMal DecidisteMall = new DecidisteMal();
+                DecidisteMall.Show();
+                this.Hide();
+                //this.Dispose();
+            }
+            if (cmbSabores.Text == "Vainilla")
+            {
+                lblResultado.Text = "O sos muy tranquilo o tenés 4 años.";
+            }
+            if (cmbSabores.Text == "Otro")
+            {
+                lblResultado.Text = "Bueno no hay más, conformate y elegí de la lista que te doy, no me pagan lo suficiente para esto";
+            }
+        }
+
+       
+    }
+}
